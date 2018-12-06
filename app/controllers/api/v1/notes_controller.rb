2cef96c94f27be9f36bfa9cffc6cb165220526aa
braj011 <bhagyaraj01@gmail.com>
2018-12-06 00:43:45 +0000

    partial styling - nokogiri is left to do

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::NotesController < ApplicationController
     user_stock = UserStock.find_by(user_id: get_current_user.id, stock_id: params[:stock_id].to_i)
     @note = Note.create(title: params[:title], content: params[:content], article_url: params[:article_url], 
             notif_date: params[:notif_date], user_stock_id: user_stock.id)
-    formatted_Date = @note.notif_date.to_datetime 
+    # formatted_Date = @note.notif_date.to_datetime 
     NoteReminderJob.perform_later(@note, params[:notif_date])  # this works - 5th Dec 11 am   # '5th Dec 2018 11:25:15'
     render json: { title: @note.title, content: @note.content, article_url: @note.article_url, notif_date: @note.notif_date,  user_stock_id: @note.user_stock_id, updated_at: @note.updated_at }
 
